0155e5edd37f2f478d1252ec9119c24f7c923657
jrryzh <1393769743@qq.com>
2022-10-09 16:00:04 +0800

    second try on pj1a (prepared for a third try)

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -6,93 +6,102 @@ public class ArrayDeque<T> {
     private int nextLast;
     private static int RFACTOR = 2;
 
-    public ArrayDeque(){
+    public ArrayDeque() {
         items = (T[]) new Object[8];
-        size = 8;
-        nextFirst = size / 2;
+        size = 0;
+        nextFirst = items.length / 2;
         nextLast = nextFirst + 1;
     }
 
-    public ArrayDeque(ArrayDeque other){
-        T[] new_items = (T[]) new Object[other.size];
-        System.arraycopy(other, 0, new_items, 0, other.size);
-        items = new_items;
+    public ArrayDeque(ArrayDeque other) {
+        T[] newItems = (T[]) new Object[other.items.length];
+        System.arraycopy(other, 0, newItems, 0, other.items.length);
+        items = newItems;
         size = other.size;
-        nextFirst = size / 2;
-        nextLast = nextFirst + 1;
+        nextFirst = other.nextFirst;
+        nextLast = other.nextLast;
     }
 
-    private void resize(int newsize){
+    private void resize(int newsize) {
         // 更新items， nextFirst, nextLast; size不变
-        T[] new_items = (T[]) new Object[newsize];
+        // 无论扩大还是缩小，应该都是填不满的，所以不用担心index越界
+        T[] newitems = (T[]) new Object[newsize];
         this.nextFirst = newsize / 4;
-        int index = this.nextFirst+1;
-        for (T item: this.items){
-            new_items[index] = item;
+        int index = this.nextFirst + 1;
+        for (T item: this.items) {
+            newitems[index] = item;
             index += 1;
         }
         this.nextLast = index;
-        this.items = new_items;
+        this.items = newitems;
     }
 
-    public void addFirst(T item){
+    public void addFirst(T item) {
         // 共需要检查改变item，size， nextFirst
-        // 查看是否需要扩大
-        if (size == items.length){
-            resize(size * RFACTOR);
-        }
         items[nextFirst] = item;
+
         // 检查是否需要nextFirst到另一边
-        if (nextFirst == 0){
-            nextFirst = items.length-1;;
-        }
-        else{
+        if (nextFirst == 0) {
+            nextFirst = items.length - 1;
+        } else {
             nextFirst -= 1;
         }
+
+        // 查看是否需要扩大（size是否满了）
         size += 1;
+        if (size() == items.length) {
+            resize(size() * RFACTOR);
+        }
     }
 
-    public void addLast(T item){
-        // 共需要检查改变item，size， nextFirst
-        // 查看是否需要扩大
-        if (size == items.length){
-            resize(size * RFACTOR);
-        }
+    public void addLast(T item) {
+        // 共需要检查改变item，size， nextLast
         items[nextLast] = item;
+
         // 检查是否需要nextLast到另一边
-        if (nextLast == items.length-1){
+        if (nextLast == items.length-1) {
             nextLast = 0;
-        }
-        else{
+        } else {
             nextLast += 1;
         }
+
+        // 查看是否需要扩大
         size += 1;
+        if (size() == items.length) {
+            resize(size() * RFACTOR);
+        }
     }
 
-    public boolean isEmpty(){
-        if (size == 0){
+    public boolean isEmpty() {
+        if (size == 0) {
             return true;
-        }
-        else{
+        } else {
             return false;
         }
     }
 
-    public void printDeque(){
+    public int size(){
+        return size;
+    }
+
+    public void printDeque() {
         // 同样暂时没有考虑empty的情况
-        if (nextFirst < nextLast){
+        // first<next 说明当前顺序为从左到右 first>next 说明first跑到右边去了 （因为满了会自动扩充，可以不用考虑）
+        // first==next 还是说明first跑到右边去，然后向左移动与last都重叠了，此时还差一个item即满
+        if (nextFirst < nextLast) {
             for (int index = nextFirst+1; index < nextLast; index ++){
                 System.out.print(items[index]);
                 System.out.print(" ");
             }
             System.out.println();
-        }
-        else{
-            for (int index = nextFirst+1; index < items.length; index ++){
+        } else {
+            // 先打印first向右，到链表末尾的item们
+            for (int index = nextFirst + 1; index <= items.length - 1; index ++) {
                 System.out.print(items[index]);
                 System.out.print(" ");
             }
-            for (int index = 0; index < nextLast; index++){
+            // 再打印链表开头向右，到last的item们
+            for (int index = 0; index < nextLast; index ++) {
                 System.out.print(items[index]);
                 System.out.print(" ");
             }
@@ -100,70 +109,73 @@ public class ArrayDeque<T> {
         }
     }
 
-    public T removeFirst(){
+    public T removeFirst() {
+        // 声明变量
         T result;
 
         // 检查是否为null
-        if (this.isEmpty()){
+        if (this.isEmpty()) {
             return null;
         }
 
         // 检查是否需要nextFirst到另一边
-        if (nextFirst == items.length-1){
+        // 暂时不确定置为null是否正确，应该没问题，因为都同时指向一个地址吧
+        if (nextFirst == items.length - 1) {
+            result = items[0];
             nextFirst = 0;
-            result = items[nextFirst];
             items[nextFirst] = null;
-        }
-        else{
+        } else {
+            result = items[nextFirst + 1];
             nextFirst += 1;
-            result = items[nextFirst];
             items[nextFirst] = null;
         }
-        size -= 1;
 
-        if ((float) size / items.length < 0.25){
+        // 检查是否需要缩小array
+        size -= 1;
+        if ((float) size() / items.length < 0.25) {
             resize(items.length / RFACTOR);
         }
 
         return result;
     }
 
-    public T removeLast(){
+    public T removeLast() {
+        // 声明变量
         T result;
 
         // 检查是否为null
-        if (this.isEmpty()){
+        if (this.isEmpty()) {
             return null;
         }
 
         // 检查是否需要nextLast到另一边
-        if (nextLast == 0){
+        if (nextLast == 0) {
             nextLast = items.length-1;
             result = items[nextLast];
             items[nextLast] = null;
-        }
-        else{
+        } else {
             nextLast -= 1;
             result = items[nextLast];
             items[nextLast] = null;
         }
-        size -= 1;
 
-        if ((float) size / items.length < 0.25){
+        // 检查是否需要缩小array
+        size -= 1;
+        if ((float) size() / items.length < 0.25) {
             resize(items.length / RFACTOR);
         }
 
         return result;
     }
 
-    public T get(int index){
-        if (index > size-1){
+    public T get(int index) {
+        // 首先排除index是否超出了当前size
+        // 未超出，则须检查 first+index 是否以超出 length 而需要从头继续数
+        if (index > size() - 1) {
             return null;
-        }
-        else if(nextFirst + index + 1 > items.length-1){
-            return items[index - (items.length - nextFirst)];
-        }
-        else{
+        } else if (nextFirst + index + 1 > items.length - 1) {
+            return items[index - (items.length - 1 - nextFirst)];
+        } else {
             return items[nextFirst + index + 1];
         }
     }
--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,80 +1,80 @@
 public class LinkedListDeque<T> {
 
-    private class TNode{
+    private class TNode {
         T item;
         TNode next;
         TNode prev;
 
-        public TNode(T i, TNode n, TNode p){
+        public TNode(T i, TNode n, TNode p) {
             item = i;
             next = n;
             prev = p;
         }
     }
 
-    public TNode sentinel;
-    public int size;
+    private TNode sentinel;
+    private int size;
 
-    public LinkedListDeque(){
+    public LinkedListDeque() {
         sentinel = new TNode(null, null, null);
         sentinel.next = sentinel;
         sentinel.prev = sentinel;
         size = 0;
     }
 
-    public LinkedListDeque(LinkedListDeque other){
-        sentinel = new TNode(null, sentinel, sentinel);
+    public LinkedListDeque(LinkedListDeque other) {
+        sentinel = new TNode(null, null, null);
+        sentinel.next = sentinel;
+        sentinel.prev = sentinel;
         TNode p = other.sentinel;
-        while (p.next != other.sentinel){
+        while (p.next != other.sentinel) {
             this.addLast(p.next.item);
             p = p.next;
         }
     }
 
-    public void addFirst(T item){
-        TNode new_item = new TNode(item, sentinel.next, sentinel);
-        sentinel.next.prev = new_item;
-        sentinel.next = new_item;
+    public void addFirst(T item) {
+        TNode newItem = new TNode(item, sentinel.next, sentinel);
+        sentinel.next.prev = newItem;
+        sentinel.next = newItem;
 
         size += 1;
     }
 
-    public void addLast(T item){
-        TNode new_item = new TNode(item, sentinel, sentinel.prev);
-        sentinel.prev.next = new_item;
-        sentinel.prev = new_item;
+    public void addLast(T item) {
+        TNode newItem = new TNode(item, sentinel, sentinel.prev);
+        sentinel.prev.next = newItem;
+        sentinel.prev = newItem;
 
         size += 1;
     }
 
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         if (size == 0){
             return true;
-        }
-        else{
+        } else {
             return false;
         }
     }
 
-    public int size(){
+    public int size() {
         return size;
     }
 
-    public void printDeque(){
+    public void printDeque() {
         // 暂时没有考虑empty的情况
         TNode p = sentinel;
-        while (p.next != sentinel){
+        while (p.next != sentinel) {
             System.out.print(p.next.item);
             System.out.print(" ");
             p = p.next;
         }
-
         System.out.println();
     }
 
-    public T removeFirst(){
+    public T removeFirst() {
         // 检查是否为null
-        if (this.isEmpty()){
+        if (this.isEmpty()) {
             return null;
         }
 
@@ -89,29 +89,32 @@ public class LinkedListDeque<T> {
         return result;
     }
 
-    public T removeLast(){
+    public T removeLast() {
         // 检查是否为null
-        if (this.isEmpty()){
+        if (this.isEmpty()) {
             return null;
         }
 
-        T result = sentinel.prev.item;
+        TNode tempNode = sentinel.prev;
+        T result = tempNode.item;
         sentinel.prev = sentinel.prev.prev;
         sentinel.prev.next = sentinel;
+
+        size -= 1;
+        tempNode = null;
 
         return result;
     }
 
-    public T get(int index){
-        // 检查是否为null
-        if (this.isEmpty()){
+    public T get(int index) {
+        // 排除index超出范围的情况
+        if (index > size() - 1) {
             return null;
         }
 
         TNode p = sentinel;
-
-        while(p.next != sentinel){
-            if (index == 0){
+        while(p.next != sentinel) {
+            if (index == 0) {
                 return p.next.item;
             }
             p = p.next;
@@ -121,15 +124,15 @@ public class LinkedListDeque<T> {
         return null;
     }
 
-    private T get_helper(TNode node, int index){
-        if (index == 0){
+    private T get_helper(TNode node, int index) {
+        if (index == 0) {
             return node.next.item;
         }
         return get_helper(node.next, index-1);
     }
 
     public T getRecursive(int index) {
-        if (index > this.size){
+        if (index > size() - 1) {
             return null;
         }
         return get_helper(sentinel, index);
