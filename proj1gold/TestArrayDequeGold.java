185f1e3dab1e52adf510b6c2617fd93c6f5ab4f5
jrryzh <1393769743@qq.com>
2022-10-13 16:45:16 +0800

    first try on pj1gold

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -7,7 +7,7 @@ public class TestArrayDequeGold {
     public void testSAD() {
         ArrayDequeSolution<Integer> ads1 = new ArrayDequeSolution<>();
         StudentArrayDeque<Integer> sad1 = new StudentArrayDeque<>();
-        String errorMessage = new String();
+        String message = "";
 
         for (int i = 0; i < 500; i += 1) {
             double numberBetweenZeroAndOne = StdRandom.uniform();
@@ -15,21 +15,21 @@ public class TestArrayDequeGold {
             if (numberBetweenZeroAndOne < 0.2 && ads1.size() > 0) {
                 Integer x = ads1.removeFirst();
                 Integer y = sad1.removeFirst();
-                errorMessage.concat("removeFirst()\n");
-                assertEquals(errorMessage, x, y);
+                message += "removeFirst()\n";
+                assertEquals(message, x, y);
             } else if (numberBetweenZeroAndOne < 0.4 && ads1.size() > 0){
                 Integer x = ads1.removeLast();
                 Integer y = sad1.removeLast();
-                errorMessage.concat("removeLast()\n");
-                assertEquals(errorMessage, x, y);
+                message += "removeLast()\n";
+                assertEquals(message, x, y);
             } else if (numberBetweenZeroAndOne < 0.7) {
                 ads1.addFirst(i);
                 sad1.addFirst(i);
-                errorMessage.concat("addFirst(" + i +")\n");
+                message += "addFirst(" + i +")\n";
             } else {
                 ads1.addLast(i);
                 sad1.addLast(i);
-                errorMessage.concat("addLast(" + i +")\n");
+                message += "addLast(" + i +")\n";
             }
         }
     }
