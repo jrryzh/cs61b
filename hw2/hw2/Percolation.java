cf40f20b0118e46c7a1ce5ded5fb6d5e1baace4a
jrryzh <1393769743@qq.com>
2022-11-06 21:12:00 +0800

    fix up on efficiency and leave backflush behind

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -17,15 +17,22 @@ public class Percolation {
             }
         }
         numOpenSites = 0;
-        gridsSet = new WeightedQuickUnionUF(N*N);
+        // 额外添加两个site到最后，分别是 virtual top site 和 virtual bottom site
+        gridsSet = new WeightedQuickUnionUF(N * N + 2);
+        for (int c = 0; c < N; c += 1) {
+            gridsSet.union(c, N * N);
+            gridsSet.union(N * (N - 1) + c, N * N + 1);
+        }
     }
 
+    // 1次union操作
     private void openHelper(int row, int col, int i, int j, WeightedQuickUnionUF gridsSet) {
         if (checkBounds(row + i) && checkBounds(col + j) && isOpen(row + i, col + j))
             gridsSet.union(calc(row + i, col + j), calc(row, col));
     }
 
     // open the site (row, col) if it is not open already
+    // 4次union操作
     public void open(int row, int col) {
         validate(row, col);
         // 检查如果没有open
@@ -41,29 +48,45 @@ public class Percolation {
             numOpenSites += 1;
         }
     }
+
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
         validate(row, col);
         return grids[row][col] == 1;
     }
+
     // is the site (row, col) full?
+    // 如果逐个检查第一行全部元素，则复杂度为O(n)
+    // 因此利用 virtual top site 进行简化 检查时只需要检查与virtual top site是否相连即可
     public boolean isFull(int row, int col) {
         validate(row, col);
-        for (int c = 0; c < grids[0].length; c += 1) {
-            if (isOpen(0, c) && gridsSet.connected(calc(row, col), calc(0, c))) return true;
+//        for (int c = 0; c < grids[0].length; c += 1) {
+//            if (isOpen(0, c) && gridsSet.connected(calc(row, col), calc(0, c))) return true;
+//        }
+        int n = grids.length;
+        if (isOpen(row, col) && gridsSet.connected(calc(row, col), n * n)) {
+            return true;
+        } else {
+            return false;
         }
-        return false;
     }
+
     // number of open sites
     public int numberOfOpenSites() {
         return numOpenSites;
     }
+
     // does the system percolate?
     public boolean percolates() {
-        for (int c = 0; c < grids[0].length; c += 1) {
-            if (isFull(grids.length - 1, c)) return true;
+//        for (int c = 0; c < grids[0].length; c += 1) {
+//            if (isFull(grids.length - 1, c)) return true;
+//        }
+        int n = grids.length;
+        if (gridsSet.connected(n * n, n * n + 1)) {
+            return true;
+        } else {
+            return false;
         }
-        return false;
     }
 
     private int calc(int row, int col) {
